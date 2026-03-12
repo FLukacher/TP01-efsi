1fa472be0fec2890abe71f89493633b405e87e73
Android Studio <devandroid@ort>
2026-03-12 09:02:10 -0300

    del 5 al 7 js

--- script.js
+++ script.js
@@ -37,3 +37,66 @@ let num1 = 500;
 let num2 = 135;
 let num3 = 3454;
 console.log(maximo(num1, num2, num3))
+
+//4
+function validarPassword() {
+
+let password = "dantemix9"
+  let valido = false;
+
+  if (password.length >= 8) {
+
+    let caracteres = password.split("").map(c => Number(c));
+
+    let i = 0;
+
+    while (i < caracteres.length && !valido) {
+      if (!isNaN(caracteres[i])) {
+        valido = true;
+      }
+      i++;
+    }
+  }
+  
+  return valido;
+}
+console.log(validarPassword())
+//5
+function sumarArray(){
+    let numeros = [1, 2, 3, 4, 5909]
+    let suma = 0
+
+    for (let i = 0; i < numeros.length; i++){
+        suma += numeros[i]
+    }
+
+    return suma
+}
+ console.log(sumarArray())
+ //6
+ function mayorNumero(){
+    let numeros = [1, 345345335, 3, 4, 5909]
+    let mayor = 0
+
+    for(let i = 0; i < numeros.length; i++){
+        if(numeros[i] > mayor){
+            mayor = numeros[i]
+        }
+    }
+    return mayor
+}
+
+console.log(mayorNumero())
+function obtenerPares(){
+    let numeros = [10, 20, 35, 40, 55];
+    let pares = [];
+
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] % 2 == 0) {
+            pares.push(numeros[i]);
+        }
+    }
+
+    return pares;
+}
+console.log(obtenerPares());
